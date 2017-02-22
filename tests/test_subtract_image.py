59d058918fc98304f0e4ad939cc211a0314833df
evelinfg <efg236@inlumine.ual.es>
2017-02-22 01:03:42 +0100

    .

--- tests/test_subtract_image.py
+++ tests/test_subtract_image.py
@@ -3,13 +3,13 @@ import numpy as np
 #import pywt
 #import matplotlib.pyplot as plt
 
-image1 = cv2.imread('../input/000.png')
+image1 = cv2.imread('../images/000.png')
 #print(type(image1))
-image2 = cv2.imread('../input/001.png')
+image2 = cv2.imread('../images/001.png')
 
 #image3=np.subtract(image1-image2)
 image3=image2-image1
 
 all_zeros=np.count_nonzero(image3)
 print(all_zeros)
-cv2.imwrite('../output/PNG/subtract58_56.png',image3)
+cv2.imwrite('../images/subtract000_001.png',image3)
